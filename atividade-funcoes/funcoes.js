c21f71b77f75ebee74d0070257aa78bd42c4a89e
Giovana souza <giovanabarbie8@gmail.com>
2025-08-25 16:59:50 -0300

    Add files via upload

---
+++ atividade-funcoes/funcoes.js
@@ -0,0 +1,11 @@
+'use strict'
+
+function Calcular1() {
+    let n1 = parseInt (document.getElementById("v1").value);
+    let n2 = parseInt (document.getElementById("v2").value);
+    let op = parseFloat (document.getElementById("operacoes").value); 
+
+     
+
+    
+}
